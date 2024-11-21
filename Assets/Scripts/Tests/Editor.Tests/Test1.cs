1a5686f7339c2230a38e501e696018b9dca22c43
Rafael <btxsqdr@gmail.com>
2024-11-21 17:19:37 +0100

    adjust test

--- Assets/Scripts/Tests/Editor.Tests/Test1.cs
+++ Assets/Scripts/Tests/Editor.Tests/Test1.cs
@@ -9,24 +9,32 @@ using UnityEditor.VersionControl;
 public class Test1 {
     [Test]
     public void Test1SimplePasses() {
-
+        Assert.That(model, Is.Not.Null);
     }
 
-    MainScreenController controller;
+    // MainScreenController controller;
     MainScreenModel model;
     // MainScreenView view;
-
+    
     [SetUp]
     public void SetUp() {
         // view = Substitute.For<MainScreenView>();
+
+        // GameObject viewRoot = new GameObject();
+        
         model = Substitute.For<MainScreenModel>();
         
         // Assert.That(view, Is.Not.Null);
         Assert.That(model, Is.Not.Null);
 
-        model.Products.Returns(returnThis: new ObservableList<EpicProduct>());
-        Assert.That(model.Products, Is.Not.Null);
-
-        // controller = new MainScreenController();
+        // model.Products.Returns(returnThis: new ObservableList<EpicProduct>());
+        // Assert.That(model.Products, Is.Not.Null);
+        
+        // controller = new MainScreenController.Builder(view)
+    }
+    
+    [TearDown]
+    public void TearDown() {
+        
     }
 }
